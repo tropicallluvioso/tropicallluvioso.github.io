caef9777b8e5b08c6de143b45be35e965ea4a112
Glitch (hickory-hollow-kidney) <none>
2022-01-25 06:45:40 +0000

    😽🎋 Checkpoint ./astro.config.mjs:3168504/56

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
-  dist: './public/',
+  dist: './build/',
   devOptions: {
     hostname: '0.0.0.0',
   },
   vite: {
     server: {
       strictPort: true,
-      hmr: {
-        port: 443 // Run the websocket server on the SSL port
-      }
+      // hmr: {
+      //   port: 443 // Run the websocket server on the SSL port
+      // }
     }
   }
 });
