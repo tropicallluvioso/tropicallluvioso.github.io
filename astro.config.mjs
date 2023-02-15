1c5f18b7158a8d5bdd43c78e5340e06908f20c1f
Glitch (hickory-hollow-kidney) <none>
2023-02-15 21:38:45 +0000

    🌧💗 Checkpoint ./package.json:19308597/1546 ./public/index.html:19308597/2464 ./src/pages/index.astro:19308597/139 ./astro.config.mjs:19308597/66 ./src/styles/main.css:19308597/3647 ./.glitch-assets:19308597/13343

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
 // @ts-check
 
 // https://astro.build/config
@@ -16,6 +15,5 @@ export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
       //   port: 443 // Run the websocket server on the SSL port
       // }
     }
-  },
-  integrations: [svelte()]
+  }
 });
