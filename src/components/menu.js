db3e5e73284cbcf8688ba9ec211d1ecaa47adfa5
Glitch (hickory-hollow-kidney) <none>
2023-02-16 19:42:05 +0000

    ⏲🚵 Checkpoint ./src/pages/about.html:19308597/2727 ./src/pages/about.astro:19308597/118 ./src/styles/main.css:19308597/749 ./src/components/menu.js:19308597/314 ./src/pages/index.astro:19308597/383 ./src/pages/screenwriting.astro:19308597/47

---
+++ src/components/menu.js
@@ -0,0 +1,8 @@
+const menuButton = document.querySelector(".menu_button");
+const menuOverlay = document.querySelector(".menu_overlay");
+const bar = document.querySelectorAll(".bar");
+
+menuButton.addEventListener("click", function() {
+  menuOverlay.classList.toggle("open");
+  bar.forEach(bar => bar.classList.toggle("close"));
+});
