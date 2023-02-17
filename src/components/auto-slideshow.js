fe1dfcb906a81c2c6a5a4ab2ada6b5e66b362d1b
Glitch (hickory-hollow-kidney) <none>
2023-02-17 06:24:16 +0000

    💓🍇 Checkpoint ./src/components/auto-slideshow.js:19308597/1509 ./src/pages/strategic-comm.astro:19308597/1190 ./src/pages/index.astro:19308597/111

--- src/components/auto-slideshow.js
+++ src/components/auto-slideshow.js
@@ -1,16 +1,14 @@
----
----
-
-<script>
 const imgs = document.querySelectorAll(".container-slides .slide");
 console.log(imgs)
 
-let currentIndex = 0;
+
+var currentIndex = 0;
 let time = 3000; // default time for auto slideshow
 
 const defClass = (startPos, index) => {
   for (let i = startPos; i < imgs.length; i++) {
     imgs[i].style.display = "none";
+   
 
   }
   imgs[index].style.display = "block";
@@ -19,26 +17,15 @@ const defClass = (startPos, index) => {
 
 defClass(1, 0);
 
-/*leftArrow.addEventListener("click", function(){
-  currentIndex <= 0 ? currentIndex = imgs.length-1 : currentIndex--;
-  defClass(0, currentIndex);
-});
 
-rightArrow.addEventListener("click", function(){
-  currentIndex >= imgs.length-1 ? currentIndex = 0 : currentIndex++;
-  defClass(0, currentIndex);
-});*/
 
 const startAutoSlide = () => {
   setInterval(() => {
-    currentIndex >= imgs.length-1 ? currentIndex = 0 : currentIndex+1;
+    currentIndex >= imgs.length-1 ? currentIndex = 0 : currentIndex++;
     defClass(0, currentIndex);
-    console.log(currentIndex)
+ console.log(currentIndex)
   }, time);
  
 };
 
 startAutoSlide();
-
-// Start the slideshow
-</script>
