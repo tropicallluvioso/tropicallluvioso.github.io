5c1977030d68d3e5953316728a8039014f4b8f73
Glitch (hickory-hollow-kidney) <none>
2023-02-17 05:05:07 +0000

    👨📷 Checkpoint ./src/pages/index.astro:19308597/1647 ./src/styles/slideshow.css:19308597/1506 ./src/layouts/MainLayout.astro:19308597/5 ./src/components/auto-slideshow.js:19308597/183

--- src/components/auto-slideshow.js
+++ src/components/auto-slideshow.js
@@ -1,19 +1,17 @@
 const imgs = document.querySelectorAll(".container img");
-const dots = document.querySelectorAll(".dot i");
 const leftArrow = document.querySelector(".arrow-left");
 const rightArrow = document.querySelector(".arrow-right");
 
 let currentIndex = 0;
-let time = 5000; // default time for auto slideshow
+let time = 3000; // default time for auto slideshow
 
 const defClass = (startPos, index) => {
   for (let i = startPos; i < imgs.length; i++) {
     imgs[i].style.display = "none";
-    dots[i].classList.remove("fa-dot-circle");
-    dots[i].classList.add("fa-circle");
+
   }
   imgs[index].style.display = "block";
-  dots[index].classList.add("fa-dot-circle");
+
 };
 
 defClass(1, 0);
